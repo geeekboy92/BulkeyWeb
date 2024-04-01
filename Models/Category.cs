254a4634f0cf33fb74097c4b21a1b7c21133a780
Reza Abdolmaleki <abdolmaleki.reza@outlook.com>
2024-04-01 10:05:29 +0330

    Revert "add data annotation for primary key in Category.CS"
    This reverts commit dab60a9d362ab4f136179513eb248937e377a487.

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace BulkeyWeb.Models;
+﻿namespace BulkeyWeb.Models;
 
 public class Category
 {
-    [Key] //data annotation and next property will be primary KEY Category Table
     public int Id { get; set; }
-    [Required]
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public int DisplayOrder { get; set; }
 }

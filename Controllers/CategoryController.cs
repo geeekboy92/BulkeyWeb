4bf3eaf4d9be483e556664916de6185752f42292
Reza Abdolmaleki <abdolmaleki.reza@outlook.com>
2024-04-01 12:42:28 +0330

    fetch all data from Categories table in CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkeyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkeyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
+            var category = _context.Categories.ToList();
             return View();
         }
     }

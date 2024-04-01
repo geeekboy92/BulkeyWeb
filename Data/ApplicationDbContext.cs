2dabfad6e9937e211bd122870fb17be13470174a
Reza Abdolmaleki <abdolmaleki.reza@outlook.com>
2024-04-01 12:04:22 +0330

    add some data into Categories table by hard code for test and do migrate and then database-update command

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,4 +9,13 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Category> Categories { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, DisplayOrder = 1, Name = "Math"},
+            new Category { Id = 2, DisplayOrder = 2, Name = "SiFi" },
+            new Category { Id = 3, DisplayOrder = 3, Name = "History" },
+            new Category { Id = 4, DisplayOrder = 4, Name = "Story" });
+    }
+
 }
